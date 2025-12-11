aaa2087d7b1f808f0f4d2f035a0a94c48c6e23af
Bilzy <billygtw5@gmail.com>
2025-12-11 17:13:19 +0700

    Add files via upload

---
+++ database/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tokens: "7495110314:AAHrs7nxoyRe3Wp-01VwmmXMYzcdsVEKeIw",  // Ubah Jadi Token Bot Mu !!!
+  owner: "6737763566", // Ubah Jadi Id Mu !!!
+  port: "2000", // Ubah Jadi Port Panel Mu !!!
+  ipvps: "167.172.84.233" // Ubah Jadi Ip Vps Mu !!!
+};
